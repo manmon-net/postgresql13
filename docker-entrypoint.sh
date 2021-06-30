158d5e3714a0da767db446a22a04613a905914be
tomimalkki <32452480+tomimalkki@users.noreply.github.com>
2021-06-30 18:09:57 +0200

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,7 @@ if [ ! -d /var/lib/postgresql/data ]
 then
   /usr/lib/postgresql/13/bin/initdb -D /var/lib/postgresql/data --pwfile=<(echo "$POSTGRES_PASSWORD")
   echo "host   all             all                                     md5" >> /var/lib/postgresql/data/pg_hba.conf
-  echo "listen_addresses = '*' >> /var/lib/postgresql/data/postgresql.conf
+  echo "listen_addresses = '*'" >> /var/lib/postgresql/data/postgresql.conf
 fi
 
 /usr/lib/postgresql/13/bin/postgres -D /var/lib/postgresql/data
